6ce0e9d7dd4b2c0860e807f7a3e855adea4a626b
Josh0Sprime <jorgepro121313@hotmail.com>
2023-01-11 22:08:38 -0300

    hamburguer menu in mobile design

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  isActiveMenu: boolean = true;
+
+  accionMenu(){
+    this.isActiveMenu ? (document.getElementById('menu')?.classList.add('mover-contenedor'),
+    this.isActiveMenu = false,
+    setTimeout(() => {
+      document.getElementById('menu')?.classList.add('index')
+    }, 500)) : 
+    ((document.getElementById('menu')?.classList.remove('index')),
+    this.isActiveMenu = true,
+    setTimeout(() => {
+      document.getElementById('menu')?.classList.remove('mover-contenedor')
+    }, 100)
+    )
+  }
 }
